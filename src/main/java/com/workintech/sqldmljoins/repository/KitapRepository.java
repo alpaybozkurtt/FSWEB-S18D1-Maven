4e2ea5037a31bcb143523497c1e0b22350409e9d
Alpay Bozkurt <av.alpaybozkurt@gmail.com>
2024-11-23 15:18:19 +0300

    Redone (nextgen compatibility issue)

--- src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
+++ src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
@@ -1,4 +1,4 @@
-package com.workintech.sqldmljoins.repository;
+ package com.workintech.sqldmljoins.repository;
 
 import com.workintech.sqldmljoins.entity.Kitap;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,12 +9,12 @@ import java.util.List;
 public interface KitapRepository extends JpaRepository<Kitap, Long> {
 
     //Dram ve Hikaye türündeki kitapları listeleyin. JOIN kullanmadan yapın.
-    String QUESTION_1 = "";
+    String QUESTION_1 = "SELECT * FROM kitap WHERE turno = ( SELECT turno FROM tur WHERE REPLACE(ad, ' ','') = 'Hikaye' ) OR turno = ( SELECT turno FROM tur WHERE REPLACE(ad, ' ','') = 'Dram' );";
     @Query(value = QUESTION_1, nativeQuery = true)
     List<Kitap> findBooks();
 
 
-    String QUESTION_10 = "";
+    String QUESTION_10 = "SELECT AVG(puan) FROM kitap;";
     @Query(value = QUESTION_10, nativeQuery = true)
     Double findAvgPointOfBooks();
 
